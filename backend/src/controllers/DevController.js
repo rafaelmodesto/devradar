3742418c13801474cc7f9ddd9f900b71d7da444e
Rafael Modesto <rafaelmodesto09@gmail.com>
2020-02-28 15:12:02 -0300

    Desenvolvimento API

---
+++ backend/src/controllers/DevController.js
@@ -0,0 +1,51 @@
+const axios = require('axios');
+const Dev = require('../models/Dev');
+const parseStringAsArray = require('../utils/parseStringAsArray');
+
+//Tipo de parâmetros:
+
+//Query Params: request.query (Filtros, ordenação, paginação...)
+//Route Params: request.params (Identificar um recurso na alteração ou remoção)
+//Body: request.body (Dados para criação ou alteração de um registro)
+
+// request = tudo que vem do front - requisição do usuário
+// response = resposta/resultado da requisição
+
+module.exports = {
+
+  async index(request, response) {
+    const devs = await Dev.find();
+
+    return response.json(devs);
+  },
+
+  async store(request, response) {  
+    const { github_username, techs, latitude, longitude } = request.body;
+
+    let dev = await Dev.findOne({ github_username });
+
+    if(!dev) {
+      const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`);
+  
+      const { name = login, avatar_url, bio } = (apiResponse.data);
+    
+      const techsArray = parseStringAsArray(techs);
+    
+      const location = {
+        type: 'Point',
+        coordinates: [longitude, latitude],
+      };
+    
+      dev = await Dev.create({
+        github_username,
+        name,
+        avatar_url,
+        bio,
+        techs: techsArray,
+        location,
+      })
+    }
+  
+    return response.json(dev);
+  }
+};
